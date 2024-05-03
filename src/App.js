00774717b4ca8fb25c4b96e32824adc6660a236e
Mehmet <mathdenizi@gmail.com>
2024-05-03 04:44:36 +0200

    first routes added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-function App() {
+import React from "react";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+import Users from "./users/pages/Users";
+import NewFeed from "./feeds/pages/NewFeed";
+
+const App = () => {
   return (
-    <div className="App">
-      <p>Lets Start</p>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/feeds/new" exact>
+          <NewFeed />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/feeds/pages/NewFeed.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewFeed = () => {
+  return <h2>New feed works...!</h2>;
+};
+
+export default NewFeed;
